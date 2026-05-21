118a7de6554add65690bb7c9140c4bccc48e0e89
CorentynSTOCK <corentyn.stock@icloud.com>
2026-05-21 13:32:52 +0200

    Bump homepage cache to v37

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "escape-erezee-v36";
+const CACHE_NAME = "escape-erezee-v37";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -6,7 +6,7 @@ const APP_SHELL = [
   "./app.js",
   "./manifest.webmanifest",
   "./assets/icon.svg",
-  "./assets/logo-escape.jpg?v=36",
+  "./assets/logo-escape.jpg?v=37",
 ];
 
 self.addEventListener("install", (event) => {
