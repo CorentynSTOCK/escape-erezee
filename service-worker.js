d8088bcd7409a41d2c8a2dbcd50e17aed7bd01da
CorentynSTOCK <corentyn.stock@icloud.com>
2026-05-21 18:10:14 +0200

    Bump cache for briefing flow

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "escape-erezee-v41";
+const CACHE_NAME = "escape-erezee-v42";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -6,7 +6,7 @@ const APP_SHELL = [
   "./app.js",
   "./manifest.webmanifest",
   "./assets/icon.svg",
-  "./assets/logo-escape.jpg?v=41",
+  "./assets/logo-escape.jpg?v=42",
 ];
 
 self.addEventListener("install", (event) => {
