68a7a8389b5068b7c446c631100d9afb2444b91a
CorentynSTOCK <corentyn.stock@icloud.com>
2026-05-21 15:10:58 +0200

    Bump cache for start point update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "escape-erezee-v40";
+const CACHE_NAME = "escape-erezee-v41";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -6,7 +6,7 @@ const APP_SHELL = [
   "./app.js",
   "./manifest.webmanifest",
   "./assets/icon.svg",
-  "./assets/logo-escape.jpg?v=40",
+  "./assets/logo-escape.jpg?v=41",
 ];
 
 self.addEventListener("install", (event) => {
