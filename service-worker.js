4851f8349840664924ab472e32ce5f8ea6425ee8
CorentynSTOCK <corentyn.stock@icloud.com>
2026-05-21 12:30:04 +0200

    Bump service worker cache to v33

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "escape-erezee-v32";
+const CACHE_NAME = "escape-erezee-v33";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -6,7 +6,7 @@ const APP_SHELL = [
   "./app.js",
   "./manifest.webmanifest",
   "./assets/icon.svg",
-  "./assets/logo-escape.svg?v=32",
+  "./assets/logo-escape.svg?v=33",
 ];
 
 self.addEventListener("install", (event) => {
